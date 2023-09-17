8db01d437be31c1de2a91bdb9429c07323617465
Carter David Henning <carterdavidhenning@gmail.com>
2023-09-17 01:55:20 -0400

    Add experiments

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@ use std::{env, path};
 
 use ggez::conf::WindowMode;
 use ggez::event::{self, EventHandler};
-use ggez::graphics::{self, Color, DrawParam};
+use ggez::glam::Vec2;
+use ggez::graphics::{self, Color, DrawParam, Image, ImageFormat, ScreenImage};
 use ggez::{glam, Context, ContextBuilder, GameResult};
 
 fn main() {
@@ -41,8 +42,29 @@ fn main() {
     event::run(ctx, event_loop, my_game);
 }
 
+struct Assets {
+    menu_bg: graphics::Image,
+}
+
+impl Assets {
+    fn new(ctx: &mut Context) -> Assets {
+        Assets {
+            // menu_bg: ScreenImage::new(&ctx.gfx, None, 1.0, 1.0, 1),
+            // menu_bg: graphics::ScreenImage::new(
+            //     ctx,
+            //     Image::from_color(ctx, 32, 32, Some(Color::CYAN)),
+            //     1.0,
+            //     1.0,
+            //     1,
+            // ),
+            menu_bg: graphics::Image::from_path(&ctx.gfx, "/kenney_ui/glassPanel.png").unwrap(),
+        }
+    }
+}
+
 struct MainMenuState {
     // Your state here...
+    assets: Assets,
 }
 
 impl MainMenuState {
@@ -50,6 +72,7 @@ impl MainMenuState {
         // Load/create resources such as images here.
         MainMenuState {
             // ...
+            assets: Assets::new(ctx),
         }
     }
 }
@@ -63,11 +86,51 @@ impl EventHandler for MainMenuState {
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
         let mut canvas = graphics::Canvas::from_frame(ctx, Color::WHITE);
-        // Draw code here...
+        // let mut canvas =
+        // graphics::Canvas::from_screen_image(ctx, &self.assets.menu_bg, Color::WHITE);
+        // let mut canvas = graphics::Canvas::from_image(
+        //     &ctx.gfx,
+        //     graphics::Image::new_canvas_image(
+        //         &ctx.gfx,
+        //         ctx.gfx.surface_format(),
+        //         ctx.gfx.frame().width(),
+        //         ctx.gfx.frame().height(),
+        //         1,
+        //     ),
+        //     Some(Color::WHITE),
+        // ); // TODO: Figure out why this doesn't work
+
+        let (screen_width, screen_height) = ctx.gfx.drawable_size();
+        let (image_width, image_height) =
+            (self.assets.menu_bg.width(), self.assets.menu_bg.height());
+        let scalar = Vec2::new(
+            screen_width / image_width as f32,
+            screen_height / image_height as f32,
+        );
+
         let image1 = graphics::Image::from_path(ctx, "/kenney_ui/dotBlue.png")?;
-        // Draw an image.
         let dst = glam::Vec2::new(200.0, 200.0);
         canvas.draw(&image1, graphics::DrawParam::new().dest(dst));
+        canvas.draw(
+            &self.assets.menu_bg,
+            graphics::DrawParam::new().scale(scalar),
+        );
         canvas.finish(ctx)
+
+        // let mut canvas = graphics::Canvas::from_screen_image(
+        //     &ctx.gfx,
+        //     ScreenImage::new(&ctx.gfx, None, 0.5, 0.5, 1),
+        //     Some(Color::WHITE),
+        // );
+        // let mut canvas = graphics::Canvas::from_image(
+        //     &ctx.gfx,
+        //     graphics::Image::new_canvas_image(&ctx.gfx, ctx.gfx.surface_format(), 200, 200, 1),
+        //     Some(Color::BLACK),
+        // );
+        // canvas.draw(
+        //     &image1,
+        //     graphics::DrawParam::new().dest(Vec2::new(300.0, 300.0)),
+        // );
+        // canvas.finish(ctx)
     }
 }
