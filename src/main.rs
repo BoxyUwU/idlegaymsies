973a098954afdd1c50f7a78d58154562042bfd37
Boxy <supbscripter@gmail.com>
2023-09-17 22:13:50 +0100

    basic movement

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
 use ggez::event::{self, EventHandler};
-use ggez::graphics::{self, Color};
-use ggez::{Context, ContextBuilder, GameResult};
+use ggez::glam::Vec2;
+use ggez::graphics::{self, Color, DrawParam, Quad};
+use ggez::{conf, Context, ContextBuilder, GameResult};
 
 fn main() {
     // Make a Context.
     let (mut ctx, event_loop) = ContextBuilder::new("my_game", "Cool Game Author")
+        .window_setup(conf::WindowSetup {
+            vsync: true,
+            ..Default::default()
+        })
+        .window_mode(conf::WindowMode {
+            resizable: true,
+            ..Default::default()
+        })
         .build()
         .expect("aieee, could not create ggez context!");
 
@@ -18,26 +27,58 @@ fn main() {
 }
 
 struct MyGame {
-    // Your state here...
+    player: Player,
+}
+
+struct Player {
+    pos: Vec2,
 }
 
 impl MyGame {
     pub fn new(_ctx: &mut Context) -> MyGame {
         // Load/create resources such as images here.
         MyGame {
-            // ...
+            player: Player {
+                pos: Vec2::new(100., 100.),
+            },
         }
     }
 }
 
 impl EventHandler for MyGame {
-    fn update(&mut self, _ctx: &mut Context) -> GameResult {
+    fn update(&mut self, ctx: &mut Context) -> GameResult {
+        let kb = &ctx.keyboard;
+
+        let mut movement = Vec2::ZERO;
+        for (sc, offset) in [
+            (0x11, Vec2::new(0.0, -1.0)),
+            (0x1E, Vec2::new(-1.0, 0.0)),
+            (0x1F, Vec2::new(0.0, 1.0)),
+            (0x20, Vec2::new(1.0, 0.0)),
+        ] {
+            if kb.is_scancode_pressed(sc) {
+                movement += offset;
+            }
+        }
+        let movement = movement.normalize_or_zero() * 10.0;
+
+        self.player.pos += movement;
+
         // Update code here...
         Ok(())
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
         let mut canvas = graphics::Canvas::from_frame(ctx, Color::WHITE);
+
+        canvas.draw(
+            &Quad,
+            DrawParam::new()
+                .color(Color::RED)
+                .dest(self.player.pos)
+                .scale(Vec2::new(32., 32.)),
+        );
+
         // Draw code here...
         canvas.finish(ctx)
     }
