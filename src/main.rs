03dfef6c5ec2daab984f96a28784d3f9d20b162c
Carter David Henning <carterdavidhenning@gmail.com>
2023-09-16 21:58:10 -0400

    Add resource directory

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,24 @@
+use std::{env, path};
+
 use ggez::event::{self, EventHandler};
 use ggez::graphics::{self, Color};
-use ggez::{Context, ContextBuilder, GameResult};
+use ggez::{glam, Context, ContextBuilder, GameResult};
 
 fn main() {
+    // Resource
+    let resource_dir = if let Ok(manifest_dir) = env::var("CARGO_MANIFEST_DIR") {
+        let mut path = path::PathBuf::from(manifest_dir);
+        path.push("assets");
+        path
+    } else {
+        path::PathBuf::from("./assets")
+    };
+
+    println!("{:#?}", resource_dir);
+
     // Make a Context.
     let (mut ctx, event_loop) = ContextBuilder::new("my_game", "Cool Game Author")
+        .add_resource_path(resource_dir)
         .build()
         .expect("aieee, could not create ggez context!");
 
